78ae3d10d35062a03976cb4b0a0806425b434ed2
Bojan M Popović <bojan.m.popovic@posta.jpptt.internal>
2016-04-06 15:12:38 +0200

    no message

---
+++ DuckBehavior/Duck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckBehavior
+{
+    public abstract class Duck
+    {
+        protected FlyBehavior flyBehavior;
+        protected QuackBehavior quackBehavior;
+
+        public abstract void display();
+
+        public void performFly()
+        {
+            flyBehavior.fly();
+        }
+
+        public void performQuack()
+        {
+            quackBehavior.quack();
+        }
+
+        public void swim()
+        {
+            Console.WriteLine("All ducks float, even decoys!");
+        }
+    }
+}
